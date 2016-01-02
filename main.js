608c9a843575553e59ef14d82c880275d2c1fe15
Victor Cui <victor.cui1996@gmail.com>
2016-01-01 22:32:00 -0500

    Update main.js
    Made use of JS prompt function, fixed minor bug, and gave users option to play CGOL

--- main.js
+++ main.js
@@ -1,538 +1,190 @@
 $(document).ready(function() {
 
-	var canvas = document.getElementById("grid");
-	var context = canvas.getContext("2d");
-	var $nextStepBtn = $("#nextstep");
-	$nextStepBtn.hide();
-	//creating a 2-D array that holds the squares in my canvas
-	function create2DArr()
-	{
-		var arr = [];
-		for (var i = 0; i < canvas.height / 10; i++) {
-	    	arr[i] = [];
-	  	}
-	  	return arr;
-	}
-	var twoDarr = create2DArr();
-	//fills up my 2D array
-	function fillUp2DArr()
-	{
-		for (var i = 0; i < canvas.height / 10; i++)
-		{
-			for (var j = 0; j < canvas.height  / 10; j++)
-			{
-				twoDarr[i][j] = 0;
-			}
-		}
-	}
-	fillUp2DArr();
-	
-	//drawing the grid on the canvas
-	function makeGrid(gridPxSize, color) 
-	{
-		context.save();
-		context.lineWidth = 0.5;
-	
-		//drawing horizontal canvas lines
-		for (var i = 0; i <= canvas.height; i += gridPxSize)
-		{
-			context.beginPath();
-			context.moveTo(0, i);
-			context.lineTo(canvas.width, i);
-			context.closePath();
-			context.stroke();
-		}
-	
-		//drawing vertical canvas lines
-		for (var k = 0; k <= canvas.width; k += gridPxSize)
-		{
-			context.beginPath();
-			context.moveTo(k, 0);
-			context.lineTo(k, canvas.width);
-			context.closePath();
-			context.stroke();
-		}
-	
-		context.restore();
-	
-	}
-	
-	makeGrid(10, "black");
-	
-	//gets my square grid
-	function getRect(canvas, anEvent)
-	{
-		var rect = canvas.getBoundingClientRect();
-		return {
-			x: 1 + (anEvent.clientX - rect.left) - (anEvent.clientX - rect.left)%10,
-	        y: 1 + (anEvent.clientY - rect.top) - (anEvent.clientY - rect.top)%10
-		};
-	}
-	
-	//filling the squares in my grid
-	//sets that square in the grid to "alive"
-	function fillSquare(context, x, y)
-	{
-		context.fillStyle = "green";
-		context.fillRect(x,y,9,9);
-	}
-	//unfilling the squares in my grid
-	function killSquare(context, x, y)
-	{
-		context.fillStyle = "white";
-		context.fillRect(x,y,9,9);
-	}
-	//marks squares that have once been alive
-	function makeLightGreen(context, x, y)
-	{
-		context.fillStyle = "LightGreen";
-		context.fillRect(x,y,9,9);
-	}
-	
+	var rightBlockId = 1;
+	var numClicks = 0;
+	var Game = function(size) {
+		this.size = size;
+	};
 
-	canvas.addEventListener('click', function(evt)
-	{
-		var mousePosition = getRect(canvas, evt);
-		var mouseToGridX = Math.floor(mousePosition.y / 10);
-		var mouseToGridY = Math.floor(mousePosition.x / 10);
-		if (evt.shiftKey) {
-			//forces cell to be alive if it isn't already
-			if (twoDarr[mouseToGridX][mouseToGridY] === 0) {
-				fillSquare(context, mousePosition.x, mousePosition.y);
-				twoDarr[mouseToGridX][mouseToGridY] = 1;
-			}
-		}
-		else if (evt.ctrlKey || evt.altKey)
-		{
-			//forces cell to be dead if it isn't already
-			if (twoDarr[mouseToGridX][mouseToGridY] === 1) {
-				makeLightGreen(context, mousePosition.x, mousePosition.y);
-				twoDarr[mouseToGridX][mouseToGridY] = 0; 
-			}
-		}
-		else if (twoDarr[mouseToGridX][mouseToGridY] === 0) {
-			fillSquare(context, mousePosition.x, mousePosition.y);
-			twoDarr[mouseToGridX][mouseToGridY] = 1;
-		}
-		else
-		{
-			makeLightGreen(context, mousePosition.x, mousePosition.y);
-			twoDarr[mouseToGridX][mouseToGridY] = 0; 
-		}
-	
-	}, false);
-	
-	
-	//getting the user inputted dimensions for canvas
-	var newWidth;
-	var realWidth; //this has to at least 20
-	//listening for keyup events that change dimension of canvas
-	document.getElementById("dimensions").addEventListener("keydown", function()
-	{
-		newWidth = this.value * 10;
-		realWidth = this.value;
-	}, false); 
-	
-	//creates a new canvas (with new user dimensions) and deletes the old one
-	document.getElementById("dimensions").onkeypress = function(evt)
-	{
-		//getting the specific key that the user entered
-		if (!evt) {
-			evt = window.event;
-		}
-		var keyCode = evt.keyCode || evt.which;
-		if (keyCode =='13') {
-			//enter pressed
-			if (realWidth < 20 || realWidth > 200) {
-				alert("Must be between 20 and 200");
-			}
-			else {
-				//clear out the old canvas
-				context.clearRect(0,0, canvas.width, canvas.height);
-				canvas.height = newWidth;
-				canvas.width = newWidth;
-				makeGrid(10, "black");
-				//restructuring my 2D array to match the new Grid
-				twoDarr = create2DArr();
-				fillUp2DArr();
-				return false;
-			}
+	Game.prototype.makeDiv = function() {
+		var body = $(".divHolder");
+		for (var i = 0; i < this.size; ++i) {
+			var div = document.createElement('div');
+			div.className = 'block';
+			div.style.backgroundColor = getRandomColor();
+			body.append(div);
 		}
 	};
 
-	//user input for game parameters
-	var $neighborhoodRadiusInput = $("#changeRadius");
-	var changeLoneliness = document.getElementById("changeLoneliness");
-	var changeOverpopulation = document.getElementById("changeOverpopulation");
-	var changeGMin = document.getElementById("changeGMin");
-	var changeGMax = document.getElementById("changeGMax");
-	
-	//Default values
-	var neighborhoodRadius = parseInt($neighborhoodRadiusInput.val());
-	var loneliness = parseInt(changeLoneliness.value);
-	var overpopulation = parseInt(changeOverpopulation.value);
-	var gMin = parseInt(changeGMin.value);
-	var gMax = parseInt(changeGMax.value);
-	var speed = 200;
-	
-	function runAutomaton()
-	{
-		//need a temp array to determine which cells are destined to live
-		//original array is kept the same
-		var tempArr = [];
-		for (var i = 0; i < twoDarr.length; i++)
-		{
-			tempArr[i] = twoDarr[i].slice();
-		}
-		for (var x = 0; x < tempArr.length; x++)
-		{
-			for (var y = 0; y < tempArr[x].length; y++)
-			{
-				var count = countLiveNeighbors(x,y);
-				if (alwaysAlive) {
-					var insideNeighbors = 0;
-					for (var col = x - neighborhoodRadius; col <= (x-neighborhoodRadius) + neighborhoodRadius*2; col++)
-					{
-						for (var row = y - neighborhoodRadius; row <= (y-neighborhoodRadius) + neighborhoodRadius*2; row++)
-						{
-							if (checkCoordinates(col, row)) {
-								insideNeighbors++;
-							}
-						}
-					}
-					count += Math.pow(2*neighborhoodRadius + 1, 2) - insideNeighbors;
-					
-				}
-				//CHECK THIS
-				else if (toroidal) {
-					var wrapAroundNeighbors = 0;
-					for (var col1 = x - neighborhoodRadius; col1 <= (x-neighborhoodRadius) + neighborhoodRadius*2; col1++)
-					{
-						for (var row1 = y - neighborhoodRadius; row1 <= (y-neighborhoodRadius) + neighborhoodRadius*2; row1++)
-						{ 
-							if (!checkCoordinates(col1,row1)) {
-								var gridLen = twoDarr.length;
-								count += twoDarr[(col1 + gridLen)%gridLen][(row1 + gridLen)%gridLen];
-							}
-						}
-					}
-	
-				}
-				
-				if (twoDarr[x][y] === 1)
-				{
+	Game.prototype.displayPattern = function() {
+		var blocks = document.getElementsByClassName('block'); //all the currently displayed blocks based on the current level
+		animate(); 
 		
-					if (deathByLoneliness(count)) 
-					{
-						tempArr[x][y] = false;
-					}
-					else if (deathByOverpopulation(count))
-					{
-						tempArr[x][y] = false;
-					}
-				}
-				else
-				{
-					//currently dead cell
-					if (spawnNextGeneration(count))
-					{
-						tempArr[x][y] = 1;
-					}
-				}	
-			}
-		}
-		//know which cells are destined to live, so copy temp back into main array
-		twoDarr = [];
-		for (var k = 0; k < tempArr.length; k++)
-		{
-			twoDarr[k] = tempArr[k].slice();
-		}
-		liveOrDie();
-	}
-	//fills in the square if it's alive
-	function liveOrDie()
-	{
-		for (var x = 0; x < twoDarr.length; x++)
-		{
-			for (var y = 0; y < twoDarr[x].length; y++)
-			{
-				if (twoDarr[x][y] === 1)
-				{
-					fillSquare(context, y* 10, x * 10);
-				}
-				else if (twoDarr[x][y] === 0)
-				{
-					killSquare(context, y*10, x*10);
-				} 
-				else
-				{
-					makeLightGreen(context, y*10, x*10);
-				}
-	
-			}
-	
-		}
-	}
-	
-	//death by loneliness
-	function deathByLoneliness(x)
-	{
-		return x < loneliness;
-	}
-	
-	function deathByOverpopulation(x)
-	{
-		return x > overpopulation;
-	}
-	
-	function equilibrium(x)
-	{
-		return (x === 2 || x === 3);
-	}
-	
-	//only applies to currently dead cells
-	function spawnNextGeneration(x)
-	{
-		return (x >= gMin && x <= gMax);
-	}
-		
-	//search for neighbors within a neighborhood radius
-	function countLiveNeighbors(x, y)
-	{
-	
-		var liveNeighbors = 0;
-		for (var r = x - neighborhoodRadius; r <= (x - neighborhoodRadius) + neighborhoodRadius*2; r++)
-		{
-			for (var c = y - neighborhoodRadius; c <= (y - neighborhoodRadius) + neighborhoodRadius*2; c++)
-			{
-				if (r === x && c === y) {
-					//don't want to check myself
-					continue;
-				}
-				if (checkCoordinates(r,c) && twoDarr[r][c] === 1) {
-					liveNeighbors += 1;
-				}
-					
-			}
-		}
-		return liveNeighbors;
-	}
-	
-	function reset()
-	{
-		for (var x = 0; x < twoDarr.length; x++)
-		{
-			for (var y = 0; y < twoDarr[x].length; y++)
-			{
-				killSquare(context, y*10, x*10);
-				twoDarr[x][y] = 0;
-			}
-		}
-	}
-	
-	function randomize()
-	{
-		for (var x = 0; x < twoDarr.length; x++)
-		{
-			for (var y = 0; y < twoDarr[x].length; y++)
-			{
-				var randNum = Math.floor(Math.random() * 2);
-				if (randNum === 0) {
-					twoDarr[x][y] = 0;
-					killSquare(context, y*10, x*10);
-				}
-				else {
-					twoDarr[x][y] = 1;
-					fillSquare(context, y*10, x*10);
-				}
-				
-				
-			}
-		}
-	}
-	
-	function checkCoordinates(x, y)
-	{
-		if (x < 0 || y < 0 || x >= twoDarr.length || y >= twoDarr.length) {
-			//ALWAYS DEAD- CHANGE THIS
-			return false;
-		}
-		return true;
-	}
+	};
 
-	//run the automaton
-	var startDate = null;
-	var canAdvanceAutomaton = false, canChangeSpeed = false;
-	var paused = false;
-	var interval_timer;
-	$("#startstop").click(function() {
-		var now = new Date();
-		if (startDate === null)
-		{
-			paused = false;
-			canChangeSpeed = true;
-			startDate = now;
-			$(this).text("Stop");
-			$nextStepBtn.fadeOut("fast");	
-			interval_timer = setInterval(runAutomaton, speed); //global variable
-			canAdvanceAutomaton = true;
-		
+	/* resets the game to four blocks */
+	Game.prototype.reset = function() {
+		$('.block').remove();
+		var $score = $("#score");
+		var $level = $("#level");
+		if ($score.length === 0  && $level.length === 0) {
+			$('.info p').remove();
+			var scoreTxt = $("<p></p>").text("Score: ");
+			var score = $("<span></span>").text("0");
+			score.attr('id',"score");
+			scoreTxt.append(score);
+			var levelTxt = $("<p></p>").text("Level: ");
+			var level = $("<span></span>").text("1");
+			level.attr('id','level');
+			levelTxt.append(level);
+			$('.info').append(scoreTxt, levelTxt);
+			if (simulate !== undefined) {
+				simulate = new Game(4);
+				simulate.makeDiv();
+			} 
+
 		}
 		else {
-			paused = true;
-			clearInterval(interval_timer);
-			startDate = null;
-			$(this).text("Start");
-			//displaying next step button
-			$nextStepBtn.fadeIn(1000);
-		
+			$score.text(0);
+			$level.text(1);
+			if (simulate !== undefined) {
+				simulate = new Game(4);
+				simulate.makeDiv();
+			} 
 		}
-	});
+		rightBlockId = 0;       //reset the correct id
+		numClicks = 0;		
+	}
+
 	
-	//marks all cells as dead
-	$('#reset').click(function() {
-		reset();	
-	});
-	
-	$('#randomize').click(function() {
-		randomize();
-	});
-	
-	//advance automaton by one step
-	$('#nextstep').click(function() {
-		if (canAdvanceAutomaton) {
-			runAutomaton();
+	function animate() {
+		var blocks = document.getElementsByClassName('block'); //all the currently displayed blocks based on the current level
+		var i = 0;
+		var randIdxs = [];
+		for (var j = 0; j < blocks.length; j++) {
+			randIdxs.push(j);
 		}
-	});
-	//reset default values
-	$('#default').click(function() {
-		$neighborhoodRadiusInput.val(1);
-		neighborhoodRadius = 1;
-		changeLoneliness.value = 2;
-		loneliness = 2;
-		changeOverpopulation.value = 3;
-		overpopulation = 3;
-		changeGMin.value = 3;
-		gMin = 3;
-		changeGMax.value = 3;
-		gMax = 3;
-		alwaysAlive = false;
-		toroidal = false;
-		alwaysDead = true;
-		document.getElementById("outsideMode").innerHTML = "Current mode: Always Dead!";
-	});
-	var alwaysAlive = false, toroidal = false, alwaysDead = true;
-	//changing references outside grid
-	$('#alwaysDead').click(function() {
-		document.getElementById("outsideMode").innerHTML = "Current mode: Always Dead!";
-		alwaysDead = true;
-		alwaysAlive = false;
-		toroidal = false;
-	});
-	$('#alwaysAlive').click(function() {
-		document.getElementById("outsideMode").innerHTML = "Current mode: Always Alive!";
-		alwaysAlive = true;
-		alwaysDead = false;
-		toroidal = false;
-		
-	});
-	$('#toroidal').click(function() {
-		document.getElementById("outsideMode").innerHTML = "Current mode: Wrap Around!";
-		toroidal = true;
-		alwaysAlive = false;
-		alwaysDead = false;
-	});
-	
-	//changes speed of program according to slider
-	$('#slider').click(function() {
-		speed = parseInt($('#slider').val());
-		if (!paused && canChangeSpeed){
-			clearInterval(interval_timer);
-			interval_timer = setInterval(runAutomaton, speed);
-		}
-	
-	});
-	
-	//changes neigborhood radius
-	$("#changeRadius").keypress(function(evt) {
-		if (evt.keyCode === 13) {
-			//enter pressed 
-			var $userRadius = $(this).val();
-			if ($userRadius < 1 || $userRadius > 10) {
-				alert("You must enter a number between 1 and 10. Try again!");
-				$(this).val(1);
+		shuffle(randIdxs);
+		var id = 1; 									//used as a unique identifier for each block (for when the user clicks on a block)
+		var interval = setInterval(function() {
+			var randIdx = randIdxs[i];
+			lightUp(blocks[randIdx], id);
+			i++;
+			id++;
+			if (i >= randIdxs.length) {
+				clearInterval(interval);
 			}
-			else
-				neighborhoodRadius = $userRadius;
+		}, 600);
+	}
+
+	function playKirbySound() {
+		var audio = $('<audio autoplay></audio>');
+		audio.append('<source src= "http://rpg.hamsterrepublic.com/wiki-images/f/fb/KirbyStyleLaser.ogg" type="audio/ogg">');
+		$("#sounds").html(audio);
+
+	}
+
+	function playCollisionSound() {
+		var audio = $('<audio autoplay></audio>');
+		audio.append('<source src= "http://rpg.hamsterrepublic.com/wiki-images/2/21/Collision8-Bit.ogg" type="audio/ogg">');
+		$("#sounds").html(audio);
+	}
+
+	$('.divHolder').on('click', '.block', function() {
+		playCollisionSound();
+		numClicks++;
+		if (numClicks == simulate.size && parseInt($(this).attr('id')) === rightBlockId) {
+			if (simulate.size === 5) {
+				//user beat the game
+				$('.info p').remove();
+				var $winner = $("<p></p>").text("Congratulations, you beat the game!", "Yes or no");
+				$(".info").prepend($winner);
+				var ans = prompt("Play again?", "Answer yes or no");
+				if (ans === "yes" || ans === "Yes") {
+					simulate.reset(4);
+				}
+				else {
+					var cgol = prompt("Play a different game?", "Answer yes or no");
+					if (cgol === "yes" || cgol === "Yes") {
+						window.location.assign("http://victorcui96.github.io/conway-life/");
+					}
+				}
+				
+			}
+			else {
+				//user beat the current level
+				var $score = $("#score");
+				var currScore = parseInt($("#score").text());
+				$score.text(currScore+10);
+				var $level = $("#level");
+				$level.text(parseInt($level.text())+1);	
+				if (simulate !== undefined) {
+					$('.block').remove();
+					var newSize = simulate.size+1;
+					simulate = new Game(newSize);
+					simulate.makeDiv();
+				}		
+			}
+			rightBlockId = 0;       //reset the correct id
+			numClicks = 0;		
 		}
+
+		else if (parseInt($(this).attr('id')) !== rightBlockId) {
+			var loserMsg = document.getElementById('loser');
+			if (loserMsg !== null) {
+				return;
+			}
+			var $loserMsg = $("<p></p>").text("Sorry, you lost after ");
+			$loserMsg.attr('id', 'loser');
+			var $level = $("#level").text();
+			$loserMsg.append($level + " levels");
+			$(".info").append($loserMsg);
+			//TODO: reset game to 4 tiles
+			simulate.reset(4);
+
+		}
+		rightBlockId++;
+	}); 
+
+	function lightUp(divBlock, id) {
+		var $block = $(divBlock).addClass("lit");
+		$block.attr("id", id);
+		playKirbySound();
+		window.setTimeout(function() {
+			$block.removeClass('lit');
+		}, 300);
+	}
+
+	var simulate = new Game(4);
+	simulate.makeDiv();
+	$("#start").on('click', function() {
+		$('#loser').remove();
+		simulate.displayPattern();
 	});
-
-	//change loneliness threshold
-	document.getElementById("changeLoneliness").onkeypress = function(evt)
-	{
-		if (evt.keyCode === 13) {
-			//enter pressed
-			var input = parseInt(this.value);
-			if (input <= 0 || input > overpopulation) {
-				alert("Loneliness must be > 0 and less than overpopulation threshold");
-				this.value = 2;
-			}
-			else
-				loneliness = parseInt(this.value);
-		}
-	};
-
-	//change overpopulation threshold
-	document.getElementById("changeOverpopulation").onkeypress = function(evt)
-	{
-		if (evt.keyCode === 13) {
-			//enter pressed
-			var input = parseInt(this.value);
-			if (input < loneliness || input >= (4*neighborhoodRadius*neighborhoodRadius + 4*neighborhoodRadius)) {
-				alert("Invalid input. Try again!");
-				this.value = 3;
-			}
-			else
-				overpopulation = parseInt(this.value);
-		}
-	};  
-
-	document.getElementById("changeGMin").onkeypress = function(evt)
-	{
-		if (evt.keyCode === 13) {
-			//enter pressed
-			var input = parseInt(this.value);
-			if (input <= 0 || input > gMax) {
-				alert("Generation min must be > 0 and <= generation max threshold");
-				this.value = 3;
-
-			}
-			else
-				gMin = parseInt(this.value);
 	
-		}
-	}; 
 
-	document.getElementById("changeGMax").onkeypress = function(evt)
-	{
-		if (evt.keyCode === 13) {
-			//enter pressed
-			var input = parseInt(this.value);
-			if (input < gMin || input >= (4*neighborhoodRadius*neighborhoodRadius + 4*neighborhoodRadius)) {
-				alert("Invalid input. Try again!");
-				this.value = 3;
-			}
-			else
-				gMax = parseInt(this.value);
-			
+	function getRandomColor() {
+		var letters = '0123456789ABCDEF'.split('');
+	    var color = '#';
+	    for (var i = 0; i < 6; i++ ) {
+	        color += letters[Math.floor(Math.random() * 16)];
+	    }
+	    return color;
+	}
+   
+    /* Fisher-Yates Shuffle */
+	function shuffle(array) {
+		var currentIndex = array.length, temporaryValue, randomIndex;
+		// While there remain elements to shuffle...
+		while (0 !== currentIndex) {
+			// Pick a remaining element...
+		    randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+			// And swap it with the current element.
+			temporaryValue = array[currentIndex];
+			array[currentIndex] = array[randomIndex];
+			array[randomIndex] = temporaryValue;
 		}
-	};  
-	//want game to be already set up when page loads
-	randomize();
-
+		  return array;
+	}
 });
-
-
-
-
-
