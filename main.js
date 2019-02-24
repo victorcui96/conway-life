a718844bd263bb856b0fd0ee977b63cb3c3c6d27
Victor Cui <victor.cui1996@gmail.com>
2019-02-23 23:53:52 -0800

    remove const, refactor functions, and add eslint.

--- main.js
+++ main.js
@@ -4,6 +4,29 @@ $(document).ready(function() {
 	const squareWidth = 9;
 	const $nextStepBtn = $("#nextstep");
 	$nextStepBtn.hide();
+
+	//user input for game parameters
+	const $neighborhoodRadiusInput = $("#changeRadius");
+	const lonelinessInput = document.getElementById("changeLoneliness");
+	const overpopulationInput = document.getElementById("changeOverpopulation");
+	const generationMinInput = document.getElementById("changeGMin");
+	const generationMaxInput = document.getElementById("changeGMax");
+	
+	//Default values
+	let neighborhoodRadius = parseInt($neighborhoodRadiusInput.val());
+	let loneliness = parseInt(lonelinessInput.value);
+	let overpopulation = parseInt(overpopulationInput.value);
+	let gMin = parseInt(generationMinInput.value);
+	let gMax = parseInt(generationMaxInput.value);
+	let speed = 200;
+
+	let canvasWidth, userWidth;
+
+	//variables that control the automaton
+	let canAdvanceAutomaton = false, canChangeSpeed = false, paused = false;
+	let interval_timer;
+	let alwaysAlive = false, toroidal = false
+
     // create a 2-D array that holds the squares in my canvas
 	const createLogicalGrid = canvas => {
 		let arr = [];
@@ -13,7 +36,7 @@ $(document).ready(function() {
 		}
 		return arr;
 	}
-	const logicalGrid = createLogicalGrid(canvas);
+	let logicalGrid = createLogicalGrid(canvas);
 	
 	//drawing the grid on the canvas
 	const makeGrid = (gridPxSize, color) => {
@@ -79,7 +102,6 @@ $(document).ready(function() {
 		}
 	}, false);
 
-	let canvasWidth, userWidth;
 	//listening for keydown events that change dimension of canvas
 	document.getElementById("dimensions").addEventListener("keydown", function() {
 		canvasWidth = this.value * 10;
@@ -97,169 +119,98 @@ $(document).ready(function() {
 			//enter pressed
 			if (userWidth < 20 || userWidth > 200) {
 				alert("Grid dimensions must be between 20 and 200");
-			}
-			else {
+			} else {
 				//clear out the old canvas
 				context.clearRect(0,0, canvas.width, canvas.height);
 				canvas.height = canvasWidth;
 				canvas.width = canvasWidth;
 				makeGrid(10, "black");
 				//restructuring my 2D array to match the new Grid
-				createLogicalGrid = create2DArr();
-				fillUp2DArr();
+				logicalGrid = createLogicalGrid(canvas);
 				return false;
 			}
 		}
 	};
 	
-
-	//user input for game parameters
-	var $neighborhoodRadiusInput = $("#changeRadius");
-	var changeLoneliness = document.getElementById("changeLoneliness");
-	var changeOverpopulation = document.getElementById("changeOverpopulation");
-	var changeGMin = document.getElementById("changeGMin");
-	var changeGMax = document.getElementById("changeGMax");
-	
-	//Default values
-	var neighborhoodRadius = parseInt($neighborhoodRadiusInput.val());
-	var loneliness = parseInt(changeLoneliness.value);
-	var overpopulation = parseInt(changeOverpopulation.value);
-	var gMin = parseInt(changeGMin.value);
-	var gMax = parseInt(changeGMax.value);
-	var speed = 200;
-	
 	const runAutomaton = logicalGrid => {
-		console.log("run automata");
 		//need a temp array to determine which cells are destined to live
 		//original array is kept the same
-		var tempArr = [];
-		for (var i = 0; i < logicalGrid.length; i++)
-		{
-			tempArr[i] = logicalGrid[i].slice();
-		}
-		for (var x = 0; x < tempArr.length; x++)
-		{
-			for (var y = 0; y < tempArr[x].length; y++)
-			{
-				var count = countLiveNeighbors(x,y, logicalGrid);
+		let tempArr = logicalGrid.map((arr) => {
+			return arr.slice();
+		});
+		for (let x = 0; x < tempArr.length; x++) {
+			for (let y = 0; y < tempArr[x].length; y++) {
+				let count = countLiveNeighbors(x,y, logicalGrid);
 				if (alwaysAlive) {
-					var insideNeighbors = 0;
-					for (var col = x - neighborhoodRadius; col <= (x-neighborhoodRadius) + neighborhoodRadius*2; col++)
-					{
-						for (var row = y - neighborhoodRadius; row <= (y-neighborhoodRadius) + neighborhoodRadius*2; row++)
-						{
-							if (checkCoordinates(col, row)) {
+					let insideNeighbors = 0;
+					for (let col = x - neighborhoodRadius; col <= (x-neighborhoodRadius) + neighborhoodRadius*2; col++) {
+						for (let row = y - neighborhoodRadius; row <= (y-neighborhoodRadius) + neighborhoodRadius*2; row++) {
+							if (checkCoordinates(col, row, logicalGrid)) {
 								insideNeighbors++;
 							}
 						}
 					}
 					count += Math.pow(2*neighborhoodRadius + 1, 2) - insideNeighbors;
-					
-				}
-				//CHECK THIS
-				else if (toroidal) {
-					var wrapAroundNeighbors = 0;
-					for (var col1 = x - neighborhoodRadius; col1 <= (x-neighborhoodRadius) + neighborhoodRadius*2; col1++)
-					{
-						for (var row1 = y - neighborhoodRadius; row1 <= (y-neighborhoodRadius) + neighborhoodRadius*2; row1++)
-						{ 
-							if (!checkCoordinates(col1,row1)) {
-								var gridLen = createLogicalGrid.length;
-								count += createLogicalGrid[(col1 + gridLen)%gridLen][(row1 + gridLen)%gridLen];
+				} else if (toroidal) {
+					for (let col = x - neighborhoodRadius; col <= (x-neighborhoodRadius) + neighborhoodRadius*2; col++) {
+						for (let row = y - neighborhoodRadius; row <= (y-neighborhoodRadius) + neighborhoodRadius*2; row++) { 
+							if (!checkCoordinates(col, row, logicalGrid)) {
+								const gridLen = logicalGrid.length;
+								count += logicalGrid[(col + gridLen)%gridLen][(row + gridLen)%gridLen];
 							}
 						}
 					}
-	
 				}
 				
-				if (logicalGrid[x][y] === 1)
-				{
-		
-					if (deathByLoneliness(count)) 
-					{
-						tempArr[x][y] = false;
-					}
-					else if (deathByOverpopulation(count))
-					{
-						tempArr[x][y] = false;
-					}
-				}
-				else
-				{
+				if (logicalGrid[x][y] === 1 && (deathByLoneliness(count) || deathByOverpopulation(count))) {
+					// mark as once been alive, but now dead
+					tempArr[x][y] = false;
+				} else {
 					//currently dead cell
-					if (spawnNextGeneration(count))
-					{
+					if (spawnNextGeneration(count)) {
 						tempArr[x][y] = 1;
 					}
 				}	
 			}
 		}
 		//know which cells are destined to live, so copy temp back into main array
-		for (var k = 0; k < tempArr.length; k++)
-		{
-			logicalGrid[k] = tempArr[k].slice();
+		for (let i = 0; i < tempArr.length; i++) {
+			logicalGrid[i] = tempArr[i].slice();
 		}
 		liveOrDie(logicalGrid, squareWidth);
 	}
 	
 	const liveOrDie = (logicalGrid, squareWidth) => {
-		for (var x = 0; x < logicalGrid.length; x++)
-		{
-			for (var y = 0; y < logicalGrid[x].length; y++)
-			{
-				if (logicalGrid[x][y] === 1)
-				{
+		for (let x = 0; x < logicalGrid.length; x++) {
+			for (let y = 0; y < logicalGrid[x].length; y++) {
+				if (logicalGrid[x][y] === 1) {
 					// fills in the square if it's alive
 					fillSquare(context, y* 10, x * 10, squareWidth);
-				}
-				else if (logicalGrid[x][y] === 0)
-				{
+				} else if (logicalGrid[x][y] === 0) {
 					killSquare(context, y*10, x*10, squareWidth);
-				} 
-				else
-				{
+				} else {
 					markOnceAliveSquares(context, y*10, x*10, squareWidth);
 				}
-	
 			}
-	
 		}
 	}
 	
-	//death by loneliness
-	const deathByLoneliness = (x) => {
-		return x < loneliness;
-	}
-	
-	function deathByOverpopulation(x)
-	{
-		return x > overpopulation;
-	}
-	
-	function equilibrium(x)
-	{
-		return (x === 2 || x === 3);
-	}
-	
+	const deathByLoneliness = x => x < loneliness;
+	const deathByOverpopulation = x => x > overpopulation;
 	//only applies to currently dead cells
-	function spawnNextGeneration(x)
-	{
-		return (x >= gMin && x <= gMax);
-	}
+	const spawnNextGeneration = x => x >= gMin && x <= gMax 
 		
 	//search for neighbors within a neighborhood radius
 	const countLiveNeighbors = (x, y, logicalGrid) => {
-		var liveNeighbors = 0;
-		for (var r = x - neighborhoodRadius; r <= (x - neighborhoodRadius) + neighborhoodRadius*2; r++)
-		{
-			for (var c = y - neighborhoodRadius; c <= (y - neighborhoodRadius) + neighborhoodRadius*2; c++)
-			{
+		let liveNeighbors = 0;
+		for (let r = x - neighborhoodRadius; r <= (x - neighborhoodRadius) + neighborhoodRadius*2; r++) {
+			for (let c = y - neighborhoodRadius; c <= (y - neighborhoodRadius) + neighborhoodRadius*2; c++) {
 				if (r === x && c === y) {
 					//don't want to check myself
 					continue;
 				}
 				if (checkCoordinates(r,c, logicalGrid) && logicalGrid[r][c] === 1) {
-					liveNeighbors += 1;
+					liveNeighbors++;
 				}
 					
 			}
@@ -268,72 +219,50 @@ $(document).ready(function() {
 	}
 	
 	const reset = (logicalGrid, squareWidth) => {
-		for (var x = 0; x < logicalGrid.length; x++)
-		{
-			for (var y = 0; y < logicalGrid[x].length; y++)
-			{
+		for (let x = 0; x < logicalGrid.length; x++) {
+			for (let y = 0; y < logicalGrid[x].length; y++) {
 				killSquare(context, y*10, x*10, squareWidth);
 				logicalGrid[x][y] = 0;
 			}
 		}
 	}
 	
-	function randomize(logicalGrid, squareWidth)
-	{
-		for (var x = 0; x < logicalGrid.length; x++)
-		{
-			for (var y = 0; y < logicalGrid[x].length; y++)
-			{
-				var randNum = Math.floor(Math.random() * 2);
+	const randomize = (logicalGrid, squareWidth) => {
+		for (let x = 0; x < logicalGrid.length; x++) {
+			for (let y = 0; y < logicalGrid[x].length; y++) {
+				const randNum = Math.floor(Math.random() * 2);
 				if (randNum === 0) {
 					logicalGrid[x][y] = 0;
 					killSquare(context, y*10, x*10, squareWidth);
-				}
-				else {
+				} else {
 					logicalGrid[x][y] = 1;
 					fillSquare(context, y*10, x*10, squareWidth);
 				}
-				
-				
 			}
 		}
 	}
 	
-	function checkCoordinates(x, y, logicalGrid)
-	{
+	const checkCoordinates = (x, y, logicalGrid) => {
 		if (x < 0 || y < 0 || x >= logicalGrid.length || y >= logicalGrid.length) {
-			//ALWAYS DEAD- CHANGE THIS
 			return false;
 		}
 		return true;
 	}
-
-	//run the automaton
-	var startDate = null;
-	var canAdvanceAutomaton = false, canChangeSpeed = false;
-	var paused = false;
-	var interval_timer;
+	
 	$("#startstop").click(function() {
-		var now = new Date();
-		if (startDate === null)
-		{
-			paused = false;
+		if (!paused) {
+			paused = true;
 			canChangeSpeed = true;
-			startDate = now;
 			$(this).text("Stop");
 			$nextStepBtn.fadeOut("fast");	
 			interval_timer = setInterval(() => runAutomaton(logicalGrid), speed); //global variable
 			canAdvanceAutomaton = true;
-		
-		}
-		else {
-			paused = true;
+		} else {
+			paused = false;
 			clearInterval(interval_timer);
-			startDate = null;
 			$(this).text("Start");
 			//displaying next step button
 			$nextStepBtn.fadeIn(1000);
-		
 		}
 	});
 	
@@ -356,39 +285,33 @@ $(document).ready(function() {
 	$('#default').click(function() {
 		$neighborhoodRadiusInput.val(1);
 		neighborhoodRadius = 1;
-		changeLoneliness.value = 2;
+		lonelinessInput.value = 2;
 		loneliness = 2;
-		changeOverpopulation.value = 3;
+		overpopulationInput.value = 3;
 		overpopulation = 3;
-		changeGMin.value = 3;
+		generationMinInput.value = 3;
 		gMin = 3;
-		changeGMax.value = 3;
+		generationMaxInput.value = 3;
 		gMax = 3;
 		alwaysAlive = false;
 		toroidal = false;
-		alwaysDead = true;
 		document.getElementById("outsideMode").innerHTML = "Current mode: Always Dead!";
 	});
-	var alwaysAlive = false, toroidal = false, alwaysDead = true;
     // changing references outside grid
 	$('#alwaysDead').click(function() {
 		document.getElementById("outsideMode").innerHTML = "Current mode: Always Dead!";
-		alwaysDead = true;
 		alwaysAlive = false;
 		toroidal = false;
 	});
 	$('#alwaysAlive').click(function() {
 		document.getElementById("outsideMode").innerHTML = "Current mode: Always Alive!";
 		alwaysAlive = true;
-		alwaysDead = false;
 		toroidal = false;
-		
 	});
 	$('#toroidal').click(function() {
 		document.getElementById("outsideMode").innerHTML = "Current mode: Wrap Around!";
 		toroidal = true;
 		alwaysAlive = false;
-		alwaysDead = false;
 	});
 	
 	//changes speed of program according to slider
@@ -398,87 +321,75 @@ $(document).ready(function() {
 			clearInterval(interval_timer);
 			interval_timer = setInterval(runAutomaton(logicalGrid), speed);
 		}
-	
 	});
 	
 	//changes neigborhood radius
 	$("#changeRadius").keypress(function(evt) {
 		if (evt.keyCode === 13) {
 			//enter pressed 
-			var $userRadius = $(this).val();
+			const $userRadius = $(this).val();
 			if ($userRadius < 1 || $userRadius > 10) {
 				alert("You must enter a number between 1 and 10. Try again!");
 				$(this).val(1);
-			}
-			else
+			} else {
 				neighborhoodRadius = $userRadius;
+			}
 		}
 	});
 
 	//change loneliness threshold
-	document.getElementById("changeLoneliness").onkeypress = function(evt)
-	{
+	document.getElementById("changeLoneliness").onkeypress = function(evt) {
 		if (evt.keyCode === 13) {
 			//enter pressed
-			var input = parseInt(this.value);
+			const input = parseInt(this.value);
 			if (input <= 0 || input > overpopulation) {
 				alert("Loneliness must be > 0 and less than overpopulation threshold");
 				this.value = 2;
-			}
-			else
+			} else {
 				loneliness = parseInt(this.value);
+			}
 		}
 	};
 
 	//change overpopulation threshold
-	document.getElementById("changeOverpopulation").onkeypress = function(evt)
-	{
+	document.getElementById("changeOverpopulation").onkeypress = function(evt) {
 		if (evt.keyCode === 13) {
 			//enter pressed
-			var input = parseInt(this.value);
+			const input = parseInt(this.value);
 			if (input < loneliness || input >= (4*neighborhoodRadius*neighborhoodRadius + 4*neighborhoodRadius)) {
 				alert("Invalid input. Try again!");
 				this.value = 3;
-			}
-			else
+			} else {
 				overpopulation = parseInt(this.value);
+			}
 		}
 	};  
 
-	document.getElementById("changeGMin").onkeypress = function(evt)
-	{
+	document.getElementById("changeGMin").onkeypress = function(evt) {
 		if (evt.keyCode === 13) {
 			//enter pressed
-			var input = parseInt(this.value);
+			const input = parseInt(this.value);
 			if (input <= 0 || input > gMax) {
 				alert("Generation min must be > 0 and <= generation max threshold");
 				this.value = 3;
-
-			}
-			else
+			} else {
 				gMin = parseInt(this.value);
-	
+			}
 		}
 	}; 
 
-	document.getElementById("changeGMax").onkeypress = function(evt)
-	{
+	document.getElementById("changeGMax").onkeypress = function(evt) {
 		if (evt.keyCode === 13) {
 			//enter pressed
-			var input = parseInt(this.value);
+			const input = parseInt(this.value);
 			if (input < gMin || input >= (4*neighborhoodRadius*neighborhoodRadius + 4*neighborhoodRadius)) {
 				alert("Invalid input. Try again!");
 				this.value = 3;
-			}
-			else
+			} else {
 				gMax = parseInt(this.value);
-			
+			}
 		}
 	};  
 	// set up game once page loads
 	randomize(logicalGrid, squareWidth);
 });
-
-
-
-
